e57b966d32d637453cdc4906514a4b336ee57538
Caleb Clausen <coatl@inforadical.net>
2009-04-08 21:41:38 -0700

    simple and limited implementation of transformations!

---
+++ regxform.rb
@@ -0,0 +1,53 @@
+module Reg
+  module Reg
+    def xform!(datum,changed={})
+      datum or fail
+      Thread.current[:Reg_xform_session]=session={}
+      self===datum or return
+      changed.merge(session){|key,old,new| fail }
+      return datum
+    ensure
+      Thread.current[:Reg_xform_session]=nil
+    end
+  end
+
+  class Bound
+    def === other #this is a hack
+      result= @reg===other
+      session=Thread.current[:Reg_xform_session]
+      if result and session and !session.has_key? @name
+        session[@name]=other
+      end
+      return result
+    end
+  end
+
+  class BoundRef
+    def === other #this is a hack
+      session=Thread.current[:Reg_xform_session]
+      if session and session.has_key? @name
+        session[@name]==other
+      else raise NameNotBound #name not bound yet? that's an error
+      end
+    end
+  end
+
+  class NameNotBound<RuntimeError; end
+
+  class Transform
+    def === other #this is a hack
+      result= @reg===other
+      session=Thread.current[:Reg_xform_session]
+      if result and session
+        session[other.__id__]=
+          case @rep
+          when Replace::Form; @rep.fill_out_simple(other) #should handle Literals as well...
+          when Deferred; @rep.formula_value(other)
+          when BoundRef; @rep #can't replace it yet...
+          else @rep
+          end
+      end
+      return result
+    end
+  end
+end
