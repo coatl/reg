e6350d26c314aeb47f540660edbad87c7723f507
Caleb Clausen <coatl@inforadical.net>
2010-01-30 14:20:12 -0800

    warn if Array#to_s ever called during tests

--- test/test_all.rb
+++ test/test_all.rb
@@ -2,3 +2,12 @@
 require 'test/test_reg'
 require 'test/regitem_thattest'
 
+class Array
+
+  alias to_s_without_warn to_s
+
+  def to_s
+    warn "calling Array#to_s from #{caller[0]}; but semantics have changed in 1.9"
+    to_s_without_warn
+  end
+end
