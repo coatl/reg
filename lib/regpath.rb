91f0d9724b560312fec289e239e1b074b5bed69d
Caleb Clausen <caleb@inforadical.net>
2016-07-02 11:59:23 -0700

    first pass at sketching out Reg::Path      this doesnt work yet

--- lib/regpath.rb
+++ lib/regpath.rb
@@ -72,15 +72,93 @@ others         n/a        single
 
 
 =end
+
+=begin
+a Reg::Path is a list of (mostly) pairs of contained in a Reg::Path constructor.
+The preferred Reg::Path constructor is the --[...] syntax.
+
+the key (left side) of each pair represents a connector matcher, and the value represents
+a value matcher. here's a list of allowed connector matchers:
+
+type   allowed connector matchers
+Object Array (+[String|Symbol, __]) , Array (+[Regexp, __]) , Reg::RespondsTo (made like -:foo or -"foo" or -:@foo)
+Hash   Any Object, Array of 1 Array|Integer, Reg::Literal, any scalar matcher
+Array  Integer,Range of Integer
+
+any reg matcher may be used as a value matcher.
+
+If the value matcher is OB and the connector matcher is of the below, the value matcher (and ** connecting to it)
+may be left off, so just the connector matcher is given:
+
+type   allowed standalone connector matchers
+Object Array (+[String|Symbol, __]) , Array (+[Regexp, __]) , Reg::RespondsTo (made like -:foo or -"foo" or -:@foo)
+Hash   Array of 1 Array|Integer, Reg::Literal, String, Symbol, Regexp
+Array  Integer,Range of Integer
+
+Reg::Paths may also contain Reg::Repeat or Reg::Subseq, provided the leaves within
+these are ultimately pairs or standalone connector matchers. 
+
+=end
+
+
 module Reg
-class Instances
-  class<<self; alias [] new end
-  
-  def initialize(method)
-    @method=method
+  class Subseq
+    def -@; Path.new @regs; end
   end
-  
-  def unwrap; @method end
+
+  class Path
+    def initialize regs
+      @regs=regs
+      expand_pairs!
+    end
+
+    def === other
+      @regs.each{|r|
+        case r
+        when Integer
+        when Range
+        when Reg::Literal
+        when Array
+          case r[0]
+          when String,Symbol
+          when Regexp
+          when Array
+          when Integer
+          end
+        when Reg::RespondsTo
+        when Reg::Reg
+        else
+        end
+      }
+    end
+  end
+
+  module Reg 
+    def expand_pairs!
+      0.upto(@regs.size-1) do |i|
+        item=@regs[i]
+        @regs[i]=
+          case item
+          when Reg::Pair; item #ignore
+          when Reg::Repeat,Reg::Subseq; item.dup.expand_pairs!
+          else Reg::Pair.new(item,OB)
+          end
+      end
+      return self
+    end
+  end
+  class Repeat
+    def expand_pairs!
+      item=@reg
+      @reg=
+          case item
+          when Reg::Pair; item #ignore
+          when Reg::Repeat,Reg::Subseq; item.dup.expand_pairs!
+          else Reg::Pair.new(item,OB)
+          end
+      return self
+    end
+  end
 
 end
-end
+
